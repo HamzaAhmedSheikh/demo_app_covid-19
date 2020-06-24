1440af2d3d3633d79ec65776803a74d0f749a717
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-25 04:03:48 +0500

    make 3 card using material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import  Cards  from './components/Cards/Cards'
+import  Chart  from './components/Chart/Chart'
+import  CountryPicker from './components/CountryPicker/CountryPicker'
 
-function App() {
+import { fetchData } from './api/index'
 
- 
+import './App.css'
+
+class App extends Component {
+
+  state = {
+    data: {}
+  }
+
+ async componentDidMount() {
+
+    const fetchedData = await fetchData()
+      
+     this.setState({data: fetchedData})
+     
+  }
+
+  
+
+ render() {
+
+   const { data } = this.state
   return (
-    <div>
-      <h1> My App </h1>     
+    <div className="container">
+      <Cards data={data} />
+      <Chart />
+      <CountryPicker />
     </div>
   );
+ }
+  
 }
 
 export default App;
