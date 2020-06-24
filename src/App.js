877ddb7d80f39ec2dd14901fab8360d90f31731f
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-25 02:34:29 +0500

    Update all things

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
 
 function App() {
+
+ 
   return (
-    <div className="App">
-      <h1> Hello World </h1>
+    <div>
+      <h1> My App </h1>     
     </div>
   );
 }
