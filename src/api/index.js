1d03429634874d27d80cbb52a1df06e93dc4515c
HamzaAhmedSheikh <hamzaahmedsheikh313@gmail.com>
2020-06-29 03:27:20 +0500

    line chart is complete

--- src/api/index.js
+++ src/api/index.js
@@ -15,3 +15,21 @@ const url = "https://covid19.mathdro.id/api"
 
     }
 }
+
+ export const fetchDailyData = async () => {
+     try {
+        const { data } = await axios.get(`${url}/daily`)  
+        
+        const modifiedData = data.map((dailyData) => ({
+            confirmed: dailyData.confirmed.total,
+            deaths: dailyData.deaths.total,
+            date: dailyData.reportDate,
+        }))
+
+          return modifiedData
+          
+
+     } catch (error) {
+         
+     }
+ }
